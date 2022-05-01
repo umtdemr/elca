731410792a9d252aa153c186f622e6d62b7b8138
umtdemr <umitde692@gmail.com>
2022-05-01 19:24:33 +0300

    added splash screen

---
+++ app/src/main/java/com/example/elca/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.elca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+}
