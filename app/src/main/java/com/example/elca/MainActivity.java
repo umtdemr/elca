49301dd5837a91face93c6f7f9515e0d0cf52dc4
umtdemr <umitde692@gmail.com>
2022-05-04 14:28:21 +0300

    dark mode support added

--- app/src/main/java/com/example/elca/MainActivity.java
+++ app/src/main/java/com/example/elca/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.elca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
     public Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +27,32 @@ public class MainActivity extends AppCompatActivity {
     private void initComponents() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sharedPreferences = getSharedPreferences("AppSettingPrefs", 0);
+        toggleDarkTheme();
+    }
+
+    private void toggleDarkTheme() {
+       boolean isNightModeOn = sharedPreferences.getBoolean("NightMode", false);
+       if (isNightModeOn) {
+           AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+       } else {
+           AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+       }
+    }
+
+    private void changeToggleThemeIcon(MenuItem menuItem, boolean isNightModeOn) {
+        if (isNightModeOn) {
+            menuItem.setIcon(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_baseline_wb_sunny_24));
+        } else {
+            menuItem.setIcon(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_baseline_nights_stay_24));
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+        changeToggleThemeIcon(menu.findItem(R.id.action_toggle_dark_theme), sharedPreferences.getBoolean("NightMode", false));
         return true;
     }
 
@@ -36,6 +62,20 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_item_list:
                 Intent intent = new Intent(MainActivity.this, ItemsListActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.action_toggle_dark_theme:
+                boolean isNightModeOn = sharedPreferences.getBoolean("NightMode", false);
+                SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+                if (isNightModeOn) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    preferencesEditor.putBoolean("NightMode", false);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    preferencesEditor.putBoolean("NightMode", true);
+                }
+                changeToggleThemeIcon(item, isNightModeOn);
+                preferencesEditor.apply();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
