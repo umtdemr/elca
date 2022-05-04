6db69fa002d65a51048b65e9df9816b8aa9b14c7
umtdemr <umitde692@gmail.com>
2022-05-04 12:49:39 +0300

    added toolbar

--- app/src/main/java/com/example/elca/MainActivity.java
+++ app/src/main/java/com/example/elca/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.elca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+
+    }
+
+    private void initComponents() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
