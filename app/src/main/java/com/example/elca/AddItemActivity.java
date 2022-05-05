a213003f6b17b42414133fb63cf119074e0c89e2
umtdemr <umitde692@gmail.com>
2022-05-05 18:27:33 +0300

    added add item acticity

---
+++ app/src/main/java/com/example/elca/AddItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.elca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
