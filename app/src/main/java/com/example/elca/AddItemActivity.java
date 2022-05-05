9e1bc503d310f505a6a7896a00df40d1f458206c
umtdemr <umitde692@gmail.com>
2022-05-05 21:56:21 +0300

    edited add form ids

--- app/src/main/java/com/example/elca/AddItemActivity.java
+++ app/src/main/java/com/example/elca/AddItemActivity.java
@@ -6,7 +6,13 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class AddItemActivity extends AppCompatActivity {
+    private boolean isValid;  // control form valid
+    private MaterialButton btnAdd;
+    private TextInputLayout txtTitleWrapper, txtMaxWattWrapper, txtAverageUsageWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +23,14 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
     private void initComponents() {
+        btnAdd = findViewById(R.id.btnAdd);
+        txtTitleWrapper = findViewById(R.id.txtTitleWrapper);
+        txtMaxWattWrapper = findViewById(R.id.txtAverageUsageWrapper);
         Toolbar toolbar = (Toolbar) findViewById(R.id.addItemToolBar);
         toolbar.setTitle(getResources().getString(R.string.menu_add_item));
         setSupportActionBar(toolbar);
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
     }}
