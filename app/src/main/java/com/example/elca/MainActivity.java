c2d391475a22f8eb6fb45558b49814050cd525f9
umtdemr <umitde692@gmail.com>
2022-05-05 18:30:25 +0300

    added constraint layout to activity

--- app/src/main/java/com/example/elca/MainActivity.java
+++ app/src/main/java/com/example/elca/MainActivity.java
@@ -119,8 +119,12 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_item_list:
-                Intent intent = new Intent(MainActivity.this, ItemsListActivity.class);
-                startActivity(intent);
+                Intent itemListIntent = new Intent(MainActivity.this, ItemsListActivity.class);
+                startActivity(itemListIntent);
+                return true;
+            case R.id.action_add_item:
+                Intent addItemIntent = new Intent(MainActivity.this, AddItemActivity.class);
+                startActivity(addItemIntent);
                 return true;
             case R.id.action_toggle_dark_theme:
                 boolean isNightModeOn = sharedPreferences.getBoolean("NightMode", false);
