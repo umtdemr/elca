0beae78315dd92ceec117d1a6e33ffa2a6ddc7dc
umtdemr <umitde692@gmail.com>
2022-05-05 16:38:41 +0300

    added load data for preview recycler view

--- app/src/main/java/com/example/elca/MainActivity.java
+++ app/src/main/java/com/example/elca/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,7 +15,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import adapter.ItemRecyclerViewAdapter;
+import model.Item;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
     private SharedPreferences sharedPreferences;
     public Toolbar toolbar;
 
@@ -22,14 +31,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initComponents();
+        loadData();
     }
 
     private void initComponents() {
+        recyclerView = findViewById(R.id.recyclerView);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         sharedPreferences = getSharedPreferences("AppSettingPrefs", 0);
+
         toggleDarkTheme();
+    }
+    private void loadData() {
+        ArrayList<Item> itemList = new ArrayList<>();
+
+        itemList.add(new Item(1, "Televizyon", 3, 50));
+        itemList.add(new Item(1, "Televizyon", 3, 50));
+        itemList.add(new Item(1, "Televizyon", 3, 50));
+        itemList.add(new Item(1, "Televizyon", 3, 50));
+        itemList.add(new Item(1, "Televizyon", 3, 50));
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setHasFixedSize(true);
+
+        ItemRecyclerViewAdapter adapter = new ItemRecyclerViewAdapter(itemList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
     }
 
     private void toggleDarkTheme() {
