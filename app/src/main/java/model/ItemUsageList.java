00a43a15fa6e6d1476d0f6d0329e83849ff38c9e
umtdemr <umitde692@gmail.com>
2022-05-06 15:58:11 +0300

    modified ItemUsageList

--- app/src/main/java/model/ItemUsageList.java
+++ app/src/main/java/model/ItemUsageList.java
@@ -5,7 +5,7 @@ import java.util.List;
 import entities.ItemEntity;
 
 public class ItemUsageList {
-    private double monthlyTotalUsage;
+    private double  weeklyTotalUsage, dailyTotalUsage, monthlyTotalUsage;
     private List<ItemUsage> itemUsages;
 
     public ItemUsageList(List<ItemEntity> items) {
@@ -13,12 +13,26 @@ public class ItemUsageList {
            itemUsages.add(new ItemUsage(items.get(i)));
        }
     }
-
+    public double getDailyTotalUsage() {
+        dailyTotalUsage = 0;
+        for (int i = 0; i < itemUsages.size(); i++) {
+            dailyTotalUsage += itemUsages.get(i).getDailyUsage();
+        }
+        return dailyTotalUsage;
+    }
+    public double getWeeklyTotalUsage() {
+        weeklyTotalUsage = 0;
+        for (int i = 0; i < itemUsages.size(); i++) {
+            weeklyTotalUsage += itemUsages.get(i).getWeeklyUsage();
+        }
+        return weeklyTotalUsage;
+    }
     public double getMonthlyTotalUsage() {
         monthlyTotalUsage = 0;
         for (int i = 0; i < itemUsages.size(); i++) {
-            ItemUsage itemUsage = itemUsages.get(i);
+            monthlyTotalUsage += itemUsages.get(i).getMonthlyUsage();
         }
         return monthlyTotalUsage;
     }
+
 }
